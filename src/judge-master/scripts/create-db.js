92ed5075092affc54ab01e5cb3ffd0878e50b6a4
WumaCoder <1829913225@qq.com>
2020-11-25 16:08:18 +0800

    数据库初始化脚本完成

---
+++ src/judge-master/scripts/create-db.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const childProcess = require('child_process');
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connect = mysql.createConnection({
+  host: process.env.TYPEORM_HOST,
+  user: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  port: process.env.TYPEORM_PORT,
+});
+
+connect.query(
+  'CREATE DATABASE `' +
+    process.env.TYPEORM_DATABASE +
+    "` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';",
+  () => {
+    connect.end();
+    console.log('create ' + process.env.TYPEORM_DATABASE + ' ok');
+  },
+);
--- src/judge-master/src/db/db.module.ts
+++ src/judge-master/src/db/db.module.ts
@@ -6,21 +6,7 @@ import { Problem } from './entities/problem';
 import { Solution } from './entities/solution';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory (configService: ConfigService) {
-      return {
-        type: 'mysql',
-        host: configService.get<string>('MYSQL_HOST'),
-        port: Number(configService.get<string>('MYSQL_PORT')),
-        username: configService.get<string>('MYSQL_USER'),
-        password: configService.get<string>('MYSQL_PASS'),
-        database: configService.get<string>('MYSQL_DB'),
-        entities: [Problem, Solution],
-        synchronize: configService.get<string>('MYSQL_SYNC') === 'true',
-      }
-    }
-  }), TypeOrmModule.forFeature([Problem, Solution])],
+  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Problem, Solution])],
   providers: [DbService],
   exports: [DbService]
 })
